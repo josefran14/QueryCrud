df729fa294f58565dd10fe1b42306dd0099b0287
josefran14 <fransaavedra21@gmail.com>
2023-07-20 21:48:03 -0400

    feat: add the table of users

--- src/components/TableUsers.jsx
+++ src/components/TableUsers.jsx
@@ -15,7 +15,42 @@ export const TableUsers = () => {
 
   return (
     <>
-      
+      <TableContainer sx={{ width: "100%", marginTop: "30px" }}>
+        <Table size="small" aria-label="a dense table">
+          <TableHead sx={{ width: "80%" }}>
+            <TableRow>
+              <TableCell>N#</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell align="center">Email</TableCell>
+              <TableCell align="right">Gender</TableCell>
+              <TableCell align="right">Username</TableCell>
+              <TableCell align="right">Website</TableCell>
+              <TableCell align="center">Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody sx={{ width: "80%" }}>
+            {data?.data.map(
+              ({ id, name, email, city, username, website }, index) => (
+                <TableRow
+                  key={id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {index + 1}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {name}
+                  </TableCell>
+                  <TableCell align="center">{email}</TableCell>
+                  <TableCell align="right">{city}</TableCell>
+                  <TableCell align="right">{username}</TableCell>
+                  <TableCell align="right">{website}</TableCell>
+                </TableRow>
+              )
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   )
 }
